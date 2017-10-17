c8484f2ebf67ea1735dc46138db60d50b71d930c
maljensen <jensenma@mit.edu>
2017-10-17 18:58:11 -0400

    modified this so that it's using a more generic way of getting the Excel files to look at, shouldn't give an error if you ahve other files in there

--- process_xls_data.m
+++ process_xls_data.m
@@ -23,9 +23,9 @@ SOFTWARE.
 %}
 
 function process_xls_data(dirname,saveName)
-% dirname = 'C:\Users\Mallory\Documents\Thesis\FCA TIDLS measurements\FZ track SRV';
-
-[fileList,fileListShort] = getAllFiles(dirname); 
+fileList = dir(fullfile(dirname,'*.xlsm')); 
+fileList = {fileList.name}';
+% [fileList,fileListShort] = getAllFiles(dirname); 
 %should be in order - 18,196,A - bottom,center,left,right,top
 
 [m,n] = size(fileList); 
@@ -33,7 +33,8 @@ dataSave = cell(m,n);
 
 for i = 1:m
     %For the NEW spreadsheet format
-    data = xlsread(fileList{i},'RawData','E4:G124');
+%     data = xlsread(fileList{i},'RawData','E4:G124');
+    data = xlsread(fullfile(dirname,fileList{i}),'RawData','E4:G124');
     lifetime = data(:,1); %seconds
     deltan = data(:,3); %cm^-3
     dataSave{i} = [deltan,lifetime];
@@ -48,8 +49,10 @@ for i = 1:m
     loglog(deltan,lifetime,'.');
     xlabel('Excess carrier density (cm^-^3)','FontSize',20);
     ylabel('Lifetime (seconds)','FontSize',20);
-    title(fileListShort{i},'FontSize',20);
+%     title(fileListShort{i},'FontSize',20);
+    title(fileList{i},'FontSize',20);
 end
 
 %save('all_XLS_data.mat','fileListShort','dataSave');
-save(saveName,'fileListShort','dataSave');
+% save(saveName,'fileListShort','dataSave');
+save(saveName,'fileList','dataSave');
